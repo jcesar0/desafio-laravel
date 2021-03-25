94848455dcba65dae359aa5accbe868117df9723
jcesar0 <julio20.cesar02@gmail.com>
2021-03-25 17:13:12 -0600

    inseridas rotas de autenticação

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,25 @@
 <?php
+/*
+ * Quando nomear rotas com metodo GET não é necessário espeficicar na frente
+ */
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group([
+    'prefix' => 'login'
+], function () {
+    Route::get('', [LoginController::class, 'create'])->name('login');
+    Route::post('', [LoginController::class, 'login'])->name('login_post');
 });
+
+Route::group([
+    'prefix' => 'register'
+], function () {
+    Route::get('', [RegisterController::class, 'create'])->name('register');
+    Route::post('', [RegisterController::class, 'register'])->name('register_post');
+});
+
+Route::get('', [HomeController::class, 'create'])->name('home')->middleware('auth');
