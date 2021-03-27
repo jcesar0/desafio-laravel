57fec4eb6ee9805f2ffdcf47aff4f085843bd4b9
jcesar0 <julio20.cesar02@gmail.com>
2021-03-26 21:37:13 -0600

    docs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,15 @@ class AuthController extends Controller
         return view('auth.auth');
     }
 
+    /*
+     * Será feita a verificação para checar se há um user logado, caso sim irá dar logout
+     * tentará encontrar um email se não encontrar irá retornar para rota de login com uma msg erro
+     * caso encontre irá fazer a verificação das senhas se não forem iguais irá retornar para rota login com uma msg erro
+     * se passar pelas instruções acima irá logar o user com remember ativo e o redirecionar para home
+     *
+     * obs: deixei 2 msg de erro iguais (quando não econtra o email e quando a senha não confere)
+     * para caso de que alguma atualização necessite de informar se o email está errado tenha que apenas trocar a msg
+     */
     public function login(LoginRequest $request)
     {
         if (Auth::user()) Auth::logout();
@@ -32,6 +41,11 @@ class AuthController extends Controller
         return redirect()->route('home');
     }
 
+    /*
+     * Será feita a verificação para checar se há um user logado, caso sim irá dar logout
+     * Pegará os parametros do request e será criada a conta
+     * após isso redirecionará o user para rota de login
+     */
     public function register(RegisterRequest $request)
     {
         if (Auth::user()) Auth::logout();
@@ -45,9 +59,13 @@ class AuthController extends Controller
         return redirect()->route('login');
     }
 
+    /*
+     * Será verificado se não está logado, caso não esteja irá redicionar para rota login,
+     * caso contrário irá fazer logout e redicionar para rota login com uma msg de erro informando que ele está deslogado
+     */
     public function logout()
     {
-        if (!Auth::user()) return redirect()->route('home');
+        if (!Auth::user()) return redirect()->route('login');
 
         Auth::logout();
         return redirect()->route('login')->withErrors('Você está deslogado faça login');
